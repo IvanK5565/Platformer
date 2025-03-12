c5cf0d58bc2904fcd57a6942c08a27b770ae4503
Ivan Kozlovsky <ivan.kz5565@gmail.com>
2025-03-12 15:33:21 +0200

    Drawable

--- SDL2_Project2/Drawable.h
+++ SDL2_Project2/Drawable.h
@@ -5,12 +5,40 @@
 #include "State.h"
 
 namespace myGame {
-	class Drawable {
-	public:
-		virtual void render(Camera&, SDL_Renderer*) = 0;
-		virtual void act(Uint32) = 0;
-		virtual SDL_Rect getRect() = 0;
-		virtual SDL_Point getFocus() = 0;
-		virtual ~Drawable() {}
-	};
+    /**
+     * @class Drawable
+     * @brief An abstract class representing objects that can be drawn and updated in the game world.
+     */
+    class Drawable {
+    public:
+        /**
+         * @brief Renders the object using the provided camera and renderer.
+         * @param camera The camera used for rendering.
+         * @param renderer The SDL renderer.
+         */
+        virtual void render(Camera& camera, SDL_Renderer* renderer) = 0;
+
+        /**
+         * @brief Updates the object's state based on the elapsed time.
+         * @param deltaTime The time elapsed since the last update.
+         */
+        virtual void act(Uint32 deltaTime) = 0;
+
+        /**
+         * @brief Retrieves the object's bounding rectangle.
+         * @return The SDL_Rect representing the object's position and size.
+         */
+        virtual SDL_Rect getRect() = 0;
+
+        /**
+         * @brief Retrieves the focus point of the object.
+         * @return The SDL_Point representing the focus position.
+         */
+        virtual SDL_Point getFocus() = 0;
+
+        /**
+         * @brief Virtual destructor for proper cleanup in derived classes.
+         */
+        virtual ~Drawable() {}
+    };
 }
