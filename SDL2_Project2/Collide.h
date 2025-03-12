02f6243d74cb0d0933a55dec7247371fc5fa6de9
Ivan Kozlovsky <ivan.kz5565@gmail.com>
2025-03-12 15:31:48 +0200

    Collide&Data

--- SDL2_Project2/Collide.h
+++ SDL2_Project2/Collide.h
@@ -5,88 +5,165 @@
 
 namespace myGame {
 
-	class Entity;
-	class Rectangle;
+    class Entity;
+    class Rectangle;
 
-	struct CollisionInfo {
-		bool collision;
-		int x1; //distance between right side A and left side B
-		//### ###
-		//#A* *B#
-		//### ###
-		     
-		int x2; //distance between left side A and right side B
-		//### ###
-		//*A# #B *
-		//### ###
-		     
-		int y1; //distance between bottom side A and top side B
-		//### #*#
-		//#A# #B#
-		//#*# ###
-		     
-		int y2; //distance between top side A and bottom side B
-		//#*# ###
-		//#A# #B#
-		//### #*#
-		explicit operator bool() const {
-			return collision;
-		}
-	};
+    /**
+     * @struct CollisionInfo
+     * @brief Stores information about a collision between objects.
+     */
+    struct CollisionInfo {
+        bool collision; ///< Indicates whether a collision occurred.
+        int x1; ///< Distance between the right side of A and the left side of B.
+        int x2; ///< Distance between the left side of A and the right side of B.
+        int y1; ///< Distance between the bottom side of A and the top side of B.
+        int y2; ///< Distance between the top side of A and the bottom side of B.
 
+        /**
+         * @brief Converts CollisionInfo to a boolean value.
+         * @return True if a collision occurred, false otherwise.
+         */
+        explicit operator bool() const {
+            return collision;
+        }
+    };
 
-	class Collide {
-	public:
-		int x, y;
-		Collide(int x, int y) : x(x), y(y) {}
-		void setPos(int, int);
-		virtual ~Collide() {}
-		virtual CollisionInfo checkCollision(const Rectangle&) = 0;
-		virtual CollisionInfo checkCollision(const int _x, const int _y) = 0;
-		virtual void drawBorder(SDL_Renderer*, Camera&) = 0;
-		virtual CollideType getType() = 0;
-	};
-		//    ..#
-		//    .##
-		//    ###
-	class RightTriangle : public Collide {
-	public:
-		static const int SIDE = 32;
-		RightTriangle(int _x, int _y) : Collide(_x, _y) {}
-		virtual ~RightTriangle() {}
-		virtual CollisionInfo checkCollision(const Rectangle&) override;
-		virtual CollisionInfo checkCollision(const int _x, const int _y) override;
-		virtual void drawBorder(SDL_Renderer*, Camera&) override;
-		virtual CollideType getType() override;
-	};
-		//    #..
-		//    ##.
-		//    ###
-	class LeftTriangle : public Collide {
-	public:
-		static const int SIDE = 32;
-		LeftTriangle(int _x, int _y) : Collide(_x, _y) {}
-		virtual ~LeftTriangle() {}
-		virtual CollisionInfo checkCollision(const Rectangle&) override;
-		virtual CollisionInfo checkCollision(const int _x, const int _y) override;
-		virtual void drawBorder(SDL_Renderer*, Camera&) override;
-		virtual CollideType getType() override;
-	};
+    /**
+     * @class Collide
+     * @brief Base class for collision detection.
+     */
+    class Collide {
+    public:
+        int x, y; ///< Position of the object.
 
-	class Rectangle : public Collide {
-	public:
-		int width, height;
-		Rectangle(int x, int y, int w, int h) : Collide(x, y), width{ w }, height{ h } {}
-		Rectangle(SDL_Rect rect) : Collide(rect.x, rect.y), width(rect.w), height(rect.h) {}
-		virtual ~Rectangle() {}
-		virtual CollisionInfo checkCollision(const Rectangle&) override;
-		virtual CollisionInfo checkCollision(const int _x, const int _y) override;
-		virtual void drawBorder(SDL_Renderer*, Camera&) override;
-		virtual CollideType getType() override;
-		SDL_Rect getRect() {
-			return { x,y,width,height };
-		}
-		friend class RightTriangle;
-		friend class LeftTriangle;
-	};
+        /**
+         * @brief Constructor.
+         * @param x Initial X position.
+         * @param y Initial Y position.
+         */
+        Collide(int x, int y) : x(x), y(y) {}
+
+        /**
+         * @brief Sets the object's position.
+         * @param newX New X position.
+         * @param newY New Y position.
+         */
+        void setPos(int newX, int newY);
+
+        virtual ~Collide() {}
+
+        /**
+         * @brief Checks for collision with a rectangle.
+         * @param rect The rectangle to check collision with.
+         * @return Collision information.
+         */
+        virtual CollisionInfo checkCollision(const Rectangle& rect) = 0;
+
+        /**
+         * @brief Checks for collision at a specific point.
+         * @param _x X coordinate.
+         * @param _y Y coordinate.
+         * @return Collision information.
+         */
+        virtual CollisionInfo checkCollision(const int _x, const int _y) = 0;
+
+        /**
+         * @brief Draws the collision border.
+         * @param renderer SDL renderer.
+         * @param camera The camera view.
+         */
+        virtual void drawBorder(SDL_Renderer* renderer, Camera& camera) = 0;
+
+        /**
+         * @brief Gets the type of collision object.
+         * @return Collision type.
+         */
+        virtual CollideType getType() = 0;
+    };
+
+    /**
+     * @class RightTriangle
+     * @brief Represents a right-angled triangle collision object.
+     */
+    class RightTriangle : public Collide {
+    public:
+        static const int SIDE = 32; ///< Side length of the triangle.
+
+        /**
+         * @brief Constructor.
+         * @param _x X position.
+         * @param _y Y position.
+         */
+        RightTriangle(int _x, int _y) : Collide(_x, _y) {}
+        virtual ~RightTriangle() {}
+
+        virtual CollisionInfo checkCollision(const Rectangle&) override;
+        virtual CollisionInfo checkCollision(const int _x, const int _y) override;
+        virtual void drawBorder(SDL_Renderer*, Camera&) override;
+        virtual CollideType getType() override;
+    };
+
+    /**
+     * @class LeftTriangle
+     * @brief Represents a left-angled triangle collision object.
+     */
+    class LeftTriangle : public Collide {
+    public:
+        static const int SIDE = 32; ///< Side length of the triangle.
+
+        /**
+         * @brief Constructor.
+         * @param _x X position.
+         * @param _y Y position.
+         */
+        LeftTriangle(int _x, int _y) : Collide(_x, _y) {}
+        virtual ~LeftTriangle() {}
+
+        virtual CollisionInfo checkCollision(const Rectangle&) override;
+        virtual CollisionInfo checkCollision(const int _x, const int _y) override;
+        virtual void drawBorder(SDL_Renderer*, Camera&) override;
+        virtual CollideType getType() override;
+    };
+
+    /**
+     * @class Rectangle
+     * @brief Represents a rectangular collision object.
+     */
+    class Rectangle : public Collide {
+    public:
+        int width, height; ///< Width and height of the rectangle.
+
+        /**
+         * @brief Constructor.
+         * @param x X position.
+         * @param y Y position.
+         * @param w Width.
+         * @param h Height.
+         */
+        Rectangle(int x, int y, int w, int h) : Collide(x, y), width(w), height(h) {}
+
+        /**
+         * @brief Constructor using an SDL_Rect.
+         * @param rect SDL rectangle.
+         */
+        Rectangle(SDL_Rect rect) : Collide(rect.x, rect.y), width(rect.w), height(rect.h) {}
+
+        virtual ~Rectangle() {}
+
+        virtual CollisionInfo checkCollision(const Rectangle&) override;
+        virtual CollisionInfo checkCollision(const int _x, const int _y) override;
+        virtual void drawBorder(SDL_Renderer*, Camera&) override;
+        virtual CollideType getType() override;
+
+        /**
+         * @brief Gets the SDL_Rect representation of the rectangle.
+         * @return SDL_Rect with position and size.
+         */
+        SDL_Rect getRect() {
+            return { x, y, width, height };
+        }
+
+        friend class RightTriangle;
+        friend class LeftTriangle;
+    };
 }
--- SDL2_Project2/Data.h
+++ SDL2_Project2/Data.h
@@ -8,149 +8,114 @@ using tinyxml2::XMLElement, tinyxml2::XMLDocument, tinyxml2::XML_SUCCESS;
 using std::string;
 
 namespace myGame {
-	struct TilesData {
-		string type, place;
-		int count, width, height;
-		bool set(XMLElement* e) {
-			type = e->Attribute("type");
-			if (type.empty()) return false;
-			place = e->Attribute("place");
-			if (place.empty()) return false;
-			if (e->QueryIntAttribute("count", &count) != XML_SUCCESS) return false;
-			if (e->QueryIntAttribute("width", &width) != XML_SUCCESS) return false;
-			if (e->QueryIntAttribute("height", &height) != XML_SUCCESS) return false;
-			return true;
-		}
-	};
-	struct StateData {
-		int count;
-		string place;
-		bool set(XMLElement* e) {
-			place = e->Attribute("place");
-			if (place.empty()) return false;
-			if (e->QueryIntAttribute("count", &count) != XML_SUCCESS) return false;
-			return true;
-		}
-	};
-	struct EntityData {
-		string type;
-		int width, height;
-		int hitW, hitH;
-		int attackW, attackH;
-		StateData attack;
-		StateData death;
-		StateData hurt;
-		StateData idle;
-		StateData jump;
-		StateData run;
-		bool set(XMLElement* e) {
-			type = e->Attribute("type");
-			if (type.empty()) return false;
-			if (e->QueryIntAttribute("width", &width) != XML_SUCCESS) return false;
-			if (e->QueryIntAttribute("height", &height) != XML_SUCCESS) return false;
-			if (e->QueryIntAttribute("hitW", &hitW) != XML_SUCCESS) return false;
-			if (e->QueryIntAttribute("hitH", &hitH) != XML_SUCCESS) return false;
-			if (e->QueryIntAttribute("attackW", &attackW) != XML_SUCCESS) return false;
-			if (e->QueryIntAttribute("attackH", &attackH) != XML_SUCCESS) return false;
+    /**
+     * @struct TilesData
+     * @brief Stores tile-related data parsed from XML.
+     */
+    struct TilesData {
+        string type, place;
+        int count, width, height;
 
-			XMLElement* e2;
+        /**
+         * @brief Sets tile data from an XML element.
+         * @param e Pointer to the XML element.
+         * @return True if data was set successfully, false otherwise.
+         */
+        bool set(XMLElement* e);
+    };
 
-			e2 = e->FirstChildElement("attack");
-			if (e2 == nullptr) return false;
-			if (!attack.set(e2)) return false;
+    /**
+     * @struct StateData
+     * @brief Stores state-related data for animations or game states.
+     */
+    struct StateData {
+        int count;
+        string place;
 
-			e2 = e->FirstChildElement("death");
-			if (e2 == nullptr) return false;
-			if (!death.set(e2)) return false;
+        /**
+         * @brief Sets state data from an XML element.
+         * @param e Pointer to the XML element.
+         * @return True if data was set successfully, false otherwise.
+         */
+        bool set(XMLElement* e);
+    };
 
-			e2 = e->FirstChildElement("hurt");
-			if (e2 == nullptr) return false;
-			if (!hurt.set(e2)) return false;
+    /**
+     * @struct EntityData
+     * @brief Stores entity-related data including animation states.
+     */
+    struct EntityData {
+        string type;
+        int width, height;
+        int hitW, hitH;
+        int attackW, attackH;
+        StateData attack;
+        StateData death;
+        StateData hurt;
+        StateData idle;
+        StateData jump;
+        StateData run;
 
-			e2 = e->FirstChildElement("idle");
-			if (e2 == nullptr) return false;
-			if (!idle.set(e2)) return false;
+        /**
+         * @brief Sets entity data from an XML element.
+         * @param e Pointer to the XML element.
+         * @return True if data was set successfully, false otherwise.
+         */
+        bool set(XMLElement* e);
+    };
 
-			e2 = e->FirstChildElement("jump");
-			if (e2 == nullptr) return false;
-			if (!jump.set(e2)) return false;
+    /**
+     * @struct MenuData
+     * @brief Stores menu-related data such as button paths and dimensions.
+     */
+    struct MenuData {
+        int width, height;
+        string playButPath;
+        string exitButPath;
 
-			e2 = e->FirstChildElement("run");
-			if (e2 == nullptr) return false;
-			if (!run.set(e2)) return false;
+        /**
+         * @brief Sets menu data from an XML element.
+         * @param e Pointer to the XML element.
+         * @return True if data was set successfully, false otherwise.
+         */
+        bool set(XMLElement* e);
+    };
 
-			return true;
+    /**
+     * @struct MapsData
+     * @brief Stores map-related data including file paths for different game states.
+     */
+    struct MapsData {
+        string type, place;
+        int count;
+        string winPath;
+        string deadPath;
 
-		}
-	};
-	struct MenuData {
-		int width, height;
-		string playButPath;
-		string exitButPath;
-		bool set(XMLElement* e) {
-			if (e->QueryIntAttribute("width", &width) != XML_SUCCESS) return false;
-			if (e->QueryIntAttribute("height", &height) != XML_SUCCESS) return false;
+        /**
+         * @brief Sets map data from an XML element.
+         * @param e Pointer to the XML element.
+         * @return True if data was set successfully, false otherwise.
+         */
+        bool set(XMLElement* e);
+    };
 
+    /**
+     * @struct Data
+     * @brief Stores all game-related data parsed from an XML document.
+     */
+    struct Data {
+        TilesData tiles;
+        EntityData player;
+        EntityData bandit[2];
+        MenuData menu;
+        MapsData maps;
+        string backgroundPath;
 
-			XMLElement* e2;
-
-			e2 = e->FirstChildElement("play");
-			if (e2 == nullptr) return false;
-			playButPath = e2->Attribute("path");
-			if (playButPath.empty()) return false;
-			
-			e2 = e->FirstChildElement("exit");
-			if (e2 == nullptr) return false;
-			exitButPath = e2->Attribute("path");
-			if (exitButPath.empty()) return false;
-
-			return true;
-		}
-
-	};
-	struct MapsData {
-		string type, place;
-		int count;
-		string winPath;
-		string deadPath;
-		bool set(XMLElement* e) {
-			type = e->Attribute("type");
-			if (type.empty()) return false;
-			place = e->Attribute("place");
-			if (place.empty()) return false;
-			if (e->QueryIntAttribute("count", &count) != XML_SUCCESS) return false;
-			winPath = e->Attribute("winPath");
-			if (winPath.empty()) return false;
-			deadPath = e->Attribute("deadPath");
-			if (deadPath.empty()) return false;
-			return true;
-		}
-	};
-	struct Data {
-		TilesData tiles;
-		EntityData player;
-		EntityData bandit[2];
-		MenuData menu;
-		MapsData maps;
-		string backgroundPath;
-		bool set(XMLDocument& d) {
-			XMLElement* data = d.FirstChildElement("data");
-			XMLElement* e;
-			e = data->FirstChildElement("tiles");
-			tiles.set(e);
-			e = data->FirstChildElement("player");
-			player.set(e);
-			e = data->FirstChildElement("bandit1");
-			bandit[0].set(e);
-			e = data->FirstChildElement("bandit2");
-			bandit[1].set(e);
-			e = data->FirstChildElement("menu");
-			menu.set(e);
-			e = data->FirstChildElement("maps");
-			maps.set(e);
-			e = data->FirstChildElement("background");
-			backgroundPath = e->Attribute("path");
-			return true;
-		}
-	};
+        /**
+         * @brief Parses and sets all game-related data from an XML document.
+         * @param d Reference to the XML document.
+         * @return True if data was set successfully, false otherwise.
+         */
+        bool set(XMLDocument& d);
+    };
 }
